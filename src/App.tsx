6092279c21921a15c33e4db72e7ee629ec25e2d4
Gean Brandao <karlos867gean@gmail.com>
2020-07-03 22:38:51 -0300

    Adicionando toast global a aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import SignIn from './pages/Signin/index';
 // import SignUp from './pages/Signup';
 import GlobalStyle from './styles/global';
+import ToastContainer from './components/ToastContainer';
 
 // eh um component colocado em volta dos componentes que queremos que tenham acesso ao contexto da aplicacao
 import { AuthProvider } from './hooks/AuthContext';
@@ -12,6 +13,8 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
